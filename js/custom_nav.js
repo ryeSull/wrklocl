4ed75137a23cab8cb3a4c899410bd135ea74edd5
Michael Hebert <michaelhebert@Michaels-MacBook-Pro-2.local>
2018-04-16 07:42:46 -0400

    JS functions account for scrolling upon home icon click event. Dark navbar behaving as it should here

--- js/custom_nav.js
+++ js/custom_nav.js
@@ -1,21 +1,40 @@
+let closeButton = document.getElementById("close-button");
+let connectIcon = document.getElementById('connect-icon');
+let navbar = document.getElementsByClassName(
+  'navbar navbar-inverse sticky-navigation navbar-fixed-top'
+)[0];
+let navClass = navbar.className;
+
 function hideNav() {
-  let closeButton = document.getElementById("close-button");
     closeButton.click();
 };
 
 $(window).scroll(function() {
-   let connectIcon = document.getElementById('connect-icon');
-   let navbar = document.getElementsByClassName(
-     'navbar navbar-inverse sticky-navigation navbar-fixed-top'
-   )[0];
-   let navClass = navbar.className;
+  let connectIcon = document.getElementById('connect-icon');
+  let navbar = document.getElementsByClassName(
+    'navbar navbar-inverse sticky-navigation navbar-fixed-top'
+  )[0];
+  let navClass = navbar.className;
 
-
-   if (navClass.includes('affix-top') === false){
-     connectIcon.style.display = 'block';
-     navbar.childNodes[1].setAttribute('id', 'want-it-darker');
-   } else {
-     connectIcon.style.display = 'none'
-     navbar.childNodes[1].setAttribute('id', '');
-   }
+  if (hideDarkTopNav.called === undefined || hideDarkTopNav.called === false) {
+    if (navClass.includes('affix-top') === false){
+      connectIcon.style.display = 'block';
+      navbar.childNodes[1].setAttribute('id', 'want-it-darker');
+    } else {
+      connectIcon.style.display = 'none'
+      navbar.childNodes[1].setAttribute('id', '');
+    }
+  } else if (hideDarkTopNav.called === true) {
+    hideDarkTopNav.called = false
+  }
 });
+
+let hideDarkTopNav = () => {
+  connectIcon.style.display = 'none'
+  navbar.childNodes[1].setAttribute('id', '');
+
+  hideDarkTopNav.called = true;
+};
+
+
+connectIcon.addEventListener('click', hideDarkTopNav);
