133fdcad26d67c6c964b28c1ff4f155097b7ed38
Michael Hebert <michaelhebert@Michaels-MacBook-Pro-2.local>
2018-04-16 10:35:21 -0400

    updates for readability. Adds call of hideTopDarkNav function to hideSideNav function

--- js/custom_nav.js
+++ js/custom_nav.js
@@ -5,27 +5,28 @@ let navbar = document.getElementsByClassName(
 )[0];
 let navClass = navbar.className;
 
-function hideNav() {
+function hideSideNav() {
     closeButton.click();
+    hideDarkTopNav();
 };
 
 $(window).scroll(function() {
-  let connectIcon = document.getElementById('connect-icon');
-  let navbar = document.getElementsByClassName(
+  let connect = document.getElementById('connect-icon');
+  let topNav = document.getElementsByClassName(
     'navbar navbar-inverse sticky-navigation navbar-fixed-top'
   )[0];
-  let navClass = navbar.className;
+  let topNavClass = navbar.className;
 
   if (hideDarkTopNav.called === undefined || hideDarkTopNav.called === false) {
-    if (navClass.includes('affix-top') === false){
-      connectIcon.style.display = 'block';
-      navbar.childNodes[1].setAttribute('id', 'want-it-darker');
-      navbar.setAttribute('id', 'want-it-darker');
+    if (topNavClass.includes('affix-top') === false){
+      connect.style.display = 'block';
+      topNav.childNodes[1].setAttribute('id', 'want-it-darker');
+      topNav.setAttribute('id', 'want-it-darker');
       $('#open-button').show();
     } else {
-      connectIcon.style.display = 'none'
-      navbar.childNodes[1].setAttribute('id', '');
-      navbar.setAttribute('id', '');
+      connect.style.display = 'none'
+      topNav.childNodes[1].setAttribute('id', '');
+      topNav.setAttribute('id', '');
       $('#open-button').hide();
     }
   } else if (hideDarkTopNav.called === true) {
